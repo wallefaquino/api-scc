86c516b1fb169e90bfa04f84798e832fb563c7b4
Wallef <wallef.fa@gmail.com>
2019-11-22 21:08:16 -0200

    First Commit

---
+++ Smart-Cattle-Control/src/main/java/com/scc/Smart/Cattle/Control/SmartCattleControlApplication.java
@@ -0,0 +1,13 @@
+package com.scc.Smart.Cattle.Control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCattleControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCattleControlApplication.class, args);
+	}
+
+}
